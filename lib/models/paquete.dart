60a20f37962d53825f1f6cb9db0ce403f1d556c6
Draxler2811 <ol1964759@gmail.com>
2024-05-27 18:23:07 -0600

    Ya quedo configurado el http y agregu codigo en models

--- lib/models/paquete.dart
+++ lib/models/paquete.dart
@@ -1,1 +1,39 @@
-//Prueba
+class Paquete {
+  double peso;
+  double largo;
+  double ancho;
+  double alto;
+  String destino;
+
+  Paquete({
+    required this.peso,
+    required this.largo,
+    required this.ancho,
+    required this.alto,
+    required this.destino,
+  });
+
+  double volumen() {
+    return largo * ancho * alto;
+  }
+
+  factory Paquete.fromJson(Map<String, dynamic> json) {
+    return Paquete(
+      peso: json['peso'],
+      largo: json['largo'],
+      ancho: json['ancho'],
+      alto: json['alto'],
+      destino: json['destino'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'peso': peso,
+      'largo': largo,
+      'ancho': ancho,
+      'alto': alto,
+      'destino': destino,
+    };
+  }
+}
--- lib/services/envio_service.dart
+++ lib/services/envio_service.dart
@@ -1,1 +1,21 @@
-//Prueba
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:calculado_a_costo_envio/models/paquete.dart';
+
+class EnvioService {
+  final String _baseUrl = 'http://localhost:8888';
+
+  Future<double> obtenerCostoEnvio(Paquete paquete) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/envio'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(paquete.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['costo'];
+    } else {
+      throw Exception('Fallo al obtener el costo de envío');
+    }
+  }
+}
